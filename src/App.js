9334968b227fc226a4e9756ef8c97773b13658fe
Donard97 <donardgolaj@gmail.com>
2021-09-21 21:58:49 +0200

    Change component to class-based

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React from 'react';
 import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <div className="calculatorSection"><Calculator /></div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = null;
+  }
+
+  render() {
+    return (
+      <div className="calculatorSection">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
