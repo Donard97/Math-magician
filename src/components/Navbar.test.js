4a89318e4a9779171321b76bcac2a2843fa8859c
Donard97 <donardgolaj@gmail.com>
2021-09-30 16:59:44 +0200

    Run tests and linters

---
+++ src/components/Navbar.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './Navbar';
+
+it('Navbar component is working', () => {
+  const tree = renderer
+    .create(
+      <Router>
+        <Navbar />
+      </Router>,
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
